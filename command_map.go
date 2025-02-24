d424da9c63942719edaf4cc25326b020a4a0b02b
Fernando Righi <43839098+whatsmynameagain@users.noreply.github.com>
2025-02-24 18:27:01 -0300

    Add response JSON structs to map command

--- command_map.go
+++ command_map.go
@@ -3,10 +3,85 @@ package main
 import "fmt"
 
 func commandMap() error {
-	locations := []string{"place 1", "place 2"}
+
+	locations := getLocations()
 	for _, location := range locations {
 		fmt.Printf("%s\n", location)
 	}
 
 	return nil
+}
+
+func getLocations() []Location {
+
+	return []Location{}
+}
+
+// actual locations
+// generated with JSON-to-GO
+type Location struct {
+	ID                   int    `json:"id"`
+	Name                 string `json:"name"`
+	GameIndex            int    `json:"game_index"`
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	Location struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Names []struct {
+		Name     string `json:"name"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+			MaxChance        int `json:"max_chance"`
+			EncounterDetails []struct {
+				MinLevel        int   `json:"min_level"`
+				MaxLevel        int   `json:"max_level"`
+				ConditionValues []any `json:"condition_values"`
+				Chance          int   `json:"chance"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+			} `json:"encounter_details"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
+// what's in the response 'results'
+type ResponseLocation struct {
+	name string `json:"name"`
+	url  string `json:"url"`
+}
+
+// response from GET
+type Response struct {
+	count    int                `json:"count"`
+	next     string             `json:"next"`
+	previous string             `json:"previous"`
+	results  []ResponseLocation `json:"results"`
 }
